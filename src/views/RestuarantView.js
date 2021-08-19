7ddda0edd034dacae9c6f5d30f2c61c01df91a34
NNJ16 <nethsera@gmail.com>
2021-08-19 22:59:58 +0530

    Add category Label to Restuarant Menu

--- src/views/RestuarantView.js
+++ src/views/RestuarantView.js
@@ -1,7 +1,7 @@
 import React from "react";
 import RestHeader from "../components/header/RestHeader";
-import './RestaurantView.css'
 import Title from "../components/header/Title";
+import CategoryLabel from "../components/Food/CategoryLabel";
 
 export default function RestaurantView(){
     return(
@@ -9,7 +9,9 @@ export default function RestaurantView(){
             <RestHeader/>
             <Title title="Order Delicious Foods" />
             <img className="headerPic" src="https://www.littlestepsasia.com/wp-content/uploads/2020/08/Best-Food-Delivery-Takeaway-Meals-In-Singapore.jpg" />
-
+            <div className="rest-body">
+                <CategoryLabel title="Specials"/>
+            </div>
         </div>
     );
 }
