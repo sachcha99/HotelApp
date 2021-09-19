a1725733266230a3d7f80f89c7e1b7e81d503ed6
mmihiranga <madhuramihiranga6@gmail.com>
2021-09-20 01:21:06 +0530

    Search Bar added

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://hotelappback.herokuapp.com/"
+    baseURL:"http://localhost:5000"
 });
 
 export default api;
